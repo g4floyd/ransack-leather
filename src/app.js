d79d41ff2c68360500eeb2e80219b3e3261b2216
Gary <gcm92.gm@gmail.com>
2017-07-10 13:20:33 -0400

    Firebase init

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use(express.static('public'))
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 })
