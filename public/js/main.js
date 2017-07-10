16db67080c9e4fac589108bb58c80e482f302121
Gary <gcm92.gm@gmail.com>
2017-07-10 14:03:45 -0400

    Recover uncommitted pre-firebase changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,14 @@
 
+document.getElementById('email-form').addEventListener('submit', function(event) {
+event.preventDefault();
+
+var email = document.getElementById('email').value;
+
+function writeUserData(email) {
+  firebase.database().ref('emails/').push({
+    email: email,
+  });
+}
+  writeUserData(email);
+
+})
